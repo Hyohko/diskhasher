fef75e97c8224e9d850280d90fa8efd0c92ac961
Hyohko <16890430+Hyohko@users.noreply.github.com>
2022-10-17 15:03:06 -0600

    Extended to the other SHA2 functions

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -46,7 +46,10 @@ struct FileData {
 enum HashAlg {
     MD5,
     SHA1,
-    SHA256
+    SHA224,
+    SHA256,
+    SHA384,
+    SHA512
 }
 
 impl Display for HashAlg {
@@ -54,7 +57,10 @@ impl Display for HashAlg {
         match self {
             Self::MD5 => write!(f, "MD5"),
             Self::SHA1 => write!(f, "SHA1"),
+            Self::SHA224 => write!(f, "SHA224"),
             Self::SHA256 => write!(f, "SHA256"),
+            Self::SHA384 => write!(f, "SHA384"),
+            Self::SHA512 => write!(f, "SHA512")
         }
     }
 }
@@ -109,6 +115,8 @@ fn main()
     }    
 }
 
+/// Recursively enumerates an absolute (canonicalized) path,
+/// returns Option<Vec<FileData>>, sorted smallest file to largest
 fn recursive_dir(abs_root_path: &Path) -> Option<Vec<FileData>> {
     let mut file_vec = Vec::<FileData>::new();
     for entry in WalkDir::new(abs_root_path)
@@ -135,6 +143,8 @@ fn recursive_dir(abs_root_path: &Path) -> Option<Vec<FileData>> {
     Some(file_vec)
 }
 
+/// Loads the expected hashes for a directory from a hashfile,
+/// expects the file format to be "<hex string> <relative file path>"
 fn load_hashes(hashfiles: &Vec<FileData>, abs_root_path: &Path) -> Result<HashMap<PathBuf, String>, String> {
     let mut hash_vec = HashMap::new();
     let hex_pattern = Regex::new(r"([[:xdigit:]]{32})|([[:xdigit:]]{48})|([[:xdigit:]]{64})").unwrap();
@@ -184,15 +194,15 @@ fn select_hasher(alg: HashAlg) -> Box<dyn DynDigest> {
     match alg {
         // HashAlg::SHA1 => Box::new(md5::Md5::default()),
         HashAlg::SHA1 => Box::new(sha1::Sha1::default()),
-        // HashAlg::SHA224 => Box::new(sha2::Sha224::default()),
+        HashAlg::SHA224 => Box::new(sha2::Sha224::default()),
         HashAlg::SHA256 => Box::new(sha2::Sha256::default()),
-        // HashAlg::SHA384 => Box::new(sha2::Sha384::default()),
-        // HashAlg::SHA512 => Box::new(sha2::Sha512::default()),
+        HashAlg::SHA384 => Box::new(sha2::Sha384::default()),
+        HashAlg::SHA512 => Box::new(sha2::Sha512::default()),
         _ => unimplemented!("unsupported digest: {}", alg),
     }
 }
 
-// thread func to do hash
+/// Thread function that opens file, hashes, and compares with expected hash
 fn perform_hash(fdata: FileData, alg: HashAlg) -> bool
 {
     // file existence check performed earlier, though we could put one here for completeness
