0fc1ac5da6db8bf5aca8310b622b91d70502f37b
Hyohko <16890430+Hyohko@users.noreply.github.com>
2023-01-18 14:38:09 -0700

    cleanup

--- cpp/execute-hbb-build.sh
+++ cpp/execute-hbb-build.sh
@@ -24,13 +24,13 @@
 # Public License along with DISKHASHER. If not, see
 # <https://www.gnu.org/licenses/>.
 
-CURDIR=$(pwd)
 HBB_NAME=phusion/holy-build-box-64
 ACTIVATE=/hbb_exe/activate-exec
 MOUNT=/io
 RUNSCRIPT=$MOUNT/compile-inside-hbb.sh
 
 if [ ! "$(docker image list | grep $HBB_NAME)" ]; then
+    echo "Holy Build Box 64 not found, pulling from the Internet"
     sudo docker pull $HBB_NAME:latest
 else
     while true; do
@@ -45,13 +45,13 @@ fi
 
 if [ ! "$(docker ps -a -q -f name=$HBB_NAME)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=$HBB_NAME)" ]; then
-        # cleanup
         docker rm $HBB_NAME:latest
     fi
-    # run your container
-    docker run -t -i --rm -v `pwd`:$MOUNT $HBB_NAME:latest $ACTIVATE bash -x -c $RUNSCRIPT
+    docker run -t -i --rm \
+        -v `pwd`:$MOUNT $HBB_NAME:latest \
+        $ACTIVATE \
+        bash -x -c $RUNSCRIPT
     strip --strip-all ./diskhasher
-    cd $CURDIR
 fi
 
 
