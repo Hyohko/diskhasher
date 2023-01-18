83b9ae2c6f699be5dff0d422ea6c0750cc47ef54
Hyohko <16890430+Hyohko@users.noreply.github.com>
2023-01-18 12:57:20 -0700

    GPL license update

--- cpp/execute-hbb-build.sh
+++ cpp/execute-hbb-build.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+# DISKHASHER v0.1 - 2022 by Hyohko
+
+##################################
+#   GPLv3 NOTICE AND DISCLAIMER
+##################################
+#
+# This file is part of DISKHASHER.
+#
+# DISKHASHER is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# DISKHASHER is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General
+# Public License along with DISKHASHER. If not, see
+# <https://www.gnu.org/licenses/>.
+
 CURDIR=$(pwd)
 HBB_NAME=phusion/holy-build-box-64
 ACTIVATE=/hbb_exe/activate-exec
