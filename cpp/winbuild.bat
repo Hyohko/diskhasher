7a78e18d8e95a4660be77194d69b52518eed7634
Hyohko <16890430+Hyohko@users.noreply.github.com>
2023-02-15 13:45:21 -0700

    first version of vs2019 build

---
+++ cpp/winbuild.bat
@@ -0,0 +1,12 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
+
+mkdir winbuild
+cd winbuild
+
+cmake -G "Visual Studio 16 2019" ..
+
+REM Build Debug mode
+msbuild .\diskhasher.sln /p:Configuration=Debug;Platform=x64 /t:Rebuild /m
+
+REM Build Release mode
+msbuild .\diskhasher.sln /p:Configuration=Release;Platform=x64 /t:Rebuild /m
